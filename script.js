025a72e3a5145a3921edbfe05f29f5cbb6c9af5b
Aaron Rivas <aarivas793@gmail.com>
2025-05-21 15:29:22 -0700

    improved logic

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const maskEmail = (email) => {
   const atIndex = email.indexOf('@');
   const domainName = email.slice(atIndex);
-  const firstChar = email.slice(0, 1);
-  const lastChar = email.slice(atIndex - 1, atIndex);
-  const toMaskString = email.slice(1, atIndex - 1);
-  let maskString = '';
-  for (let i = 0; i < toMaskString.length; i++) {
-    maskString += '*';
-  }
+  const firstChar = email[0];
+  const lastChar = email[atIndex - 1];
+  const maskLength = atIndex - 2;
+  const maskString = '*'.repeat(Math.max(0, maskLength));
   return firstChar + maskString + lastChar + domainName;
 };
 
